cc4dc5ce101c7d6298f7e1433dce2b92c4ba460d
Fajar Adie Santosa <fajardios01@gmail.com>
2026-03-07 12:14:24 +0700

    fix: api/

--- api/index.php
+++ api/index.php
@@ -1,7 +1,15 @@
 <?php
 
+// Jalankan migration & seeder hanya sekali
+$lockFile = '/tmp/migrated.lock';
 
-// Set storage path yang bisa ditulis di Vercel
-$_ENV['APP_STORAGE'] = '/tmp/storage';
+if (!file_exists($lockFile)) {
+    $projectRoot = __DIR__ . '/..';
+
+    shell_exec("cd $projectRoot && php artisan migrate --force 2>&1");
+    shell_exec("cd $projectRoot && php artisan db:seed --class=AdminSeeder --force 2>&1");
+
+    file_put_contents($lockFile, '1');
+}
 
 require __DIR__ . '/../public/index.php';
